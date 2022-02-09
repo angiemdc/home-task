1362778cac88956e5b7c5b9d2d816bf6e67d31d8
angelica_duque <angelica_duque@epam.com>
2022-02-09 11:58:36 -0500

    applied all methods of ctrating components

--- home-task-1/src/App.js
+++ home-task-1/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, PureComponent, createElement } from 'react';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class HeaderTitle extends PureComponent {
+  render() {
+    return <h1>{this.props.title}</h1>;
+  }
+}
+
+const FinalCounter = ({ counter }) => <h2>counter: {counter}</h2>;
+
+const ButtonCounter = ({ handle, text }) =>
+  createElement('button', { onClick: handle }, text);
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+      title: 'My Counter',
+    };
+  }
+
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      counter: 0,
+    });
+  };
+  render() {
+    const { counter, title } = this.state;
+    return (
+      <main>
+        <header>
+          <HeaderTitle title={title} />
+        </header>
+        <section>
+          <FinalCounter counter={counter} />
+          <div>
+            <ButtonCounter handle={this.increment} text={'Increment!'} />
+            <ButtonCounter handle={this.decrement} text={'Decrement!'} />
+            <ButtonCounter handle={this.reset} text={'Reset!'} />
+          </div>
+        </section>
+      </main>
+    );
+  }
 }
 
 export default App;
